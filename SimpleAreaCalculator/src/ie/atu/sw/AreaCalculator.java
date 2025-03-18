6544887d2b6a9dbfe8bd7a8d4a22af668caa734d
siog <siog@LAPTOP-545TJ6PE>
2025-03-18 17:47:17 +0000

    import Scanner from java.util to resolve issue in previous commit

--- SimpleAreaCalculator/src/ie/atu/sw/AreaCalculator.java
+++ SimpleAreaCalculator/src/ie/atu/sw/AreaCalculator.java
@@ -1,5 +1,8 @@
 package ie.atu.sw;
 
+import java.util.Scanner; //ctrl + shift + o to import missing library
+// java.util.* = import everything in the util directory
+
 public class AreaCalculator {
 
 	//Instance variables
